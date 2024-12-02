a4073fcb4f7428832a19d183b29a090cc3f9cb8a
Łukasz Kita <lukasz.kita0@gmail.com>
2024-12-02 12:38:41 +0100

    Add more test cases

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -1,31 +1,60 @@
-defmodule Membrane.Agora.IntegrationTest do
+defmodule Membrane.Transcoder.IntegrationTest do
   use ExUnit.Case
   import Membrane.Testing.Assertions
   import Membrane.ChildrenSpec
 
+  alias Membrane.{H264, H265, VP8, RawVideo, AAC, Opus, RawAudio}
   alias Membrane.Testing.Pipeline
+  alias Membrane.Transcoder.Support.Preprocessors
 
-  @test_cases [
-    {"input.h264", Membrane.H265},
-    {"input.h264", Membrane.H264},
-    {"input.h264", Membrane.VP8}
+  @video_inputs [
+    %{input_format: H264, input_file: "video.h264", preprocess: &Preprocessors.parse_h264/1},
+    %{input_format: RawVideo, input_file: "video.h264", preprocess: &Preprocessors.decode_h264/1},
+    %{input_format: H265, input_file: "video.h265", preprocess: &Preprocessors.parse_h265/1},
+    %{input_format: VP8, input_file: "video.ivf", preprocess: &Preprocessors.parse_vp8/1}
   ]
+  @video_outputs [RawVideo, H264, H265, VP8]
+  @video_cases for input <- @video_inputs,
+                   output <- @video_outputs,
+                   do: Map.put(input, :output_format, output)
 
-  Enum.map(@test_cases, fn {input_file, output_module} ->
-    test "if #{inspect(input_file)} stream is transcoded to #{inspect(output_module)}" do
+  @raw_audio_stream_format %RawAudio{
+    channels: 2,
+    sample_rate: 44_100,
+    sample_format: :s24le
+  }
+
+  @audio_inputs [
+    # %{
+    #   input_format: RawAudio,
+    #   input_file: "audio.raw",
+    #   preprocess: &Preprocessors.parse_raw_audio(&1, @raw_audio_stream_format)
+    # },
+    %{input_format: AAC, input_file: "audio.aac", preprocess: &Preprocessors.parse_aac/1},
+    %{input_format: Opus, input_file: "audio.opus", preprocess: &Preprocessors.parse_opus/1}
+  ]
+  @audio_outputs [RawAudio, AAC, Opus]
+  @audio_cases for input <- @audio_inputs,
+                   output <- @audio_outputs,
+                   do: Map.put(input, :output_format, output)
+
+  @test_cases @video_cases ++ @audio_cases
+
+  Enum.map(@test_cases, fn test_case ->
+    test "if #{inspect(test_case.input_format)} stream is transcoded to #{inspect(test_case.output_format)}" do
       pid = Pipeline.start_link_supervised!()
 
       spec =
         child(%Membrane.File.Source{
-          location: Path.join("./test/fixtures", unquote(input_file))
+          location: Path.join("./test/fixtures", unquote(test_case.input_file))
         })
-        |> child(%Membrane.H264.Parser{generate_best_effort_timestamps: %{framerate: {30, 1}}})
-        |> child(%Membrane.Transcoder{output_stream_format: unquote(output_module)})
+        |> then(unquote(test_case.preprocess))
+        |> child(%Membrane.Transcoder{output_stream_format: unquote(test_case.output_format)})
         |> child(:sink, Membrane.Testing.Sink)
 
       Pipeline.execute_actions(pid, spec: spec)
 
-      assert_sink_stream_format(pid, :sink, %unquote(output_module){})
+      assert_sink_stream_format(pid, :sink, %unquote(test_case.output_format){})
       Pipeline.terminate(pid)
     end
   end)
