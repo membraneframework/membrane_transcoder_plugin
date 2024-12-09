c575b5c76162d139ce3906348cc0ea665300802c
Łukasz Kita <lukasz.kita0@gmail.com>
2024-12-09 16:16:25 +0100

    Update credo and fix credo warnings

--- test/support/preprocessors.ex
+++ test/support/preprocessors.ex
@@ -1,4 +1,5 @@
 defmodule Membrane.Transcoder.Support.Preprocessors do
+  @moduledoc false
   import Membrane.ChildrenSpec
 
   @raw_audio_stream_format %Membrane.RawAudio{
@@ -7,6 +8,7 @@ defmodule Membrane.Transcoder.Support.Preprocessors do
     sample_format: :s16le
   }
 
+  @spec decode_h264(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def decode_h264(link_builder) do
     child(link_builder, %Membrane.H264.Parser{
       output_alignment: :au,
@@ -16,6 +18,7 @@ defmodule Membrane.Transcoder.Support.Preprocessors do
     |> child(%Membrane.H264.FFmpeg.Decoder{})
   end
 
+  @spec parse_h264(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_h264(link_builder) do
     child(link_builder, %Membrane.H264.Parser{
       output_alignment: :au,
@@ -24,6 +27,7 @@ defmodule Membrane.Transcoder.Support.Preprocessors do
     })
   end
 
+  @spec parse_h265(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_h265(link_builder) do
     child(link_builder, %Membrane.H265.Parser{
       output_alignment: :au,
@@ -32,10 +36,12 @@ defmodule Membrane.Transcoder.Support.Preprocessors do
     })
   end
 
+  @spec parse_vp8(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_vp8(link_builder) do
     child(link_builder, Membrane.IVF.Deserializer)
   end
 
+  @spec parse_raw_audio(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_raw_audio(link_builder) do
     child(link_builder, %Membrane.RawAudioParser{
       stream_format: @raw_audio_stream_format,
@@ -43,10 +49,12 @@ defmodule Membrane.Transcoder.Support.Preprocessors do
     })
   end
 
+  @spec parse_aac(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_aac(link_builder) do
     child(link_builder, %Membrane.AAC.Parser{out_encapsulation: :ADTS})
   end
 
+  @spec parse_opus(Membrane.ChildrenSpec.builder()) :: Membrane.ChildrenSpec.builder()
   def parse_opus(link_builder) do
     child(link_builder, %Membrane.Opus.Parser{
       input_delimitted?: true,
